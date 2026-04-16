48e8694ed87558c4dc2a29a3d5db3952827858e0
Kann96 <kann9620@gmail.com>
2026-04-16 17:27:31 +0300

    Initial Commit

---
+++ animatedObject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  animatedObject
+//
+//  Created by Анастасия Кучер on 16.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
